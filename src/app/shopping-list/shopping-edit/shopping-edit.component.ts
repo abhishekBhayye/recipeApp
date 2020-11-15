2fed06f53126149ea42913841b5ceaad0a92e1fd
bhayyeabhishek@gmail.com <bhayyeabhishek@gmail.com>
2020-11-15 20:26:52 +0000

    Shopping list module integration

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,26 @@
+import { Component, ElementRef, OnInit, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredients.module';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
+
+  @Output() ingredientsAdded = new EventEmitter<Ingredient>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.nameInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientsAdded.emit(newIngredient);
+  }
+}
